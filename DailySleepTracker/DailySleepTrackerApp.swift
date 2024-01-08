15ccaed8a650a7e38a018855e01c53d915e7b1ea
Max Roberts <maxroberts2003@gmail.com>
2024-01-08 12:20:17 -0500

    Initial Commit

---
+++ DailySleepTracker/DailySleepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailySleepTrackerApp.swift
+//  DailySleepTracker
+//
+//  Created by Max Roberts on 1/8/24.
+//
+
+import SwiftUI
+
+@main
+struct DailySleepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
